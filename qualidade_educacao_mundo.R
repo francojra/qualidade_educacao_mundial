be8dc87f67beff298b3be39f06e56292e258c65d
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-08-21 23:51:41 -0300

    Add files via upload

---
+++ qualidade_educacao_mundo.R
@@ -0,0 +1,18 @@
+
+# Qualidade da Educação no mundo -----------------------------------------------------------------------------------------------------------
+# Autora do script: Jeanne Franco ----------------------------------------------------------------------------------------------------------
+# Data: 21/08/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/quality-of-education ------------------------------------------------------------------------------
+
+# Sobre o conjunto de dados ----------------------------------------------------------------------------------------------------------------
+
+### The PISA Study
+
+### The Program for International Student Assessment (PISA) 
+### assessment, which is coordinated by the OECD, is the most well 
+### known international assessment of learning outcomes. The first 
+### PISA study was carried out in 1997 and since then it was 
+### repeated every three years.
+
+
+
